1f77ac1c66ea8fdd35191c4313eca2f29c306f76
Denzel Huijbers <huijbers15@outlook.com>
2024-03-07 14:26:26 +0100

    added login page and post login data to gateway

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import {useForm} from "react-hook-form"
+import pen from '../assets/pen-and-ink.svg'
 
 import { Button } from "@/components/ui/button"
 import {
@@ -38,15 +39,31 @@ const LoginForm = () => {
         password: "",
         },
     })
-    
+    async function postLoginData(baseUrl: string, endpoint: string, data: Object){
+      const response = await fetch(`${baseUrl}${endpoint}`, {
+        method: "POST",
+        headers: {'Accept': 'application/json', 'Content-type': 'application/json'},
+        body: JSON.stringify(data)
+      });
+
+      return response.json();
+    }
 
     function onSubmit(values: z.infer<typeof formSchema>) {
         console.log(values)
+        const baseUrl = "http://localhost:5065/api/";
+        postLoginData(baseUrl, "login", values).then(response => console.log(response))
     }
 
     return (
+      <div className='h-screen flex items-center justify-center w-3/5'>
+       
         <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-3/5 ">
+          <div className='flex flex-row justify-items-center'>
+            <h1 className='text-5xl'>Noted</h1>
+            <img className='h-[36px] w-[36px]' src={pen}></img>
+          </div>
           <FormField
             control={form.control}
             name="username"
@@ -74,9 +91,10 @@ const LoginForm = () => {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit" className=''>Submit</Button>
         </form>
       </Form>
+      </div>
     )
 }
   
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import LoginForm from './LoginForm'
+import img from '../assets/login-image.jpg'
 
 const LoginPage = () => {
   return (
-    <>
-    <div className='w-50 h-full'>
-        <img src="" alt="" className='h-full w-full'/>
+    
+    <div className='flex flex-row bg-cover'>
+        <img src={img} alt="" className='w-2-5 h-screen'/>
+        <LoginForm/>
     </div>
-    <LoginForm/>
-    </>
+    
   )
 }
 
