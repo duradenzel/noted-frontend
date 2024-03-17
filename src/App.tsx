55f40f4fdab8b976fe865ac3cf4bbacceb153312
Denzel Huijbers <huijbers15@outlook.com>
2024-03-17 13:21:51 +0100

    auto route to login when not authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@
 import './App.css'
 import HomePage from './components/pages/HomePage';
 import LoginPage from './components/pages/LoginPage';
-import ResourceTableTabs from './components/ResourceTableTabs';
-import Sidebar from './components/Sidebar';
-import { SidebarLayout } from './components/SidebarLayout';
-import { BrowserRouter, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 
+import { BrowserRouter, Navigate, Route, redirect as Router, Routes} from 'react-router-dom';
+import Redirect from 'react-router-dom';
 
 function App() {
+  
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  
 
   return (
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
+        <Route  path='/' element={isAuthenticated ? <HomePage/> : <Navigate to='/login'/>}/>
+        <Route  path='/login' element={<LoginPage/>}/>
+       
+        {/* <Route  path='/login'>
+          {isAuthenticated ? <HomePage/> : <Navigate to='/login'/>}
+        </Route> */}
       </Routes>
     </BrowserRouter>
     </>
